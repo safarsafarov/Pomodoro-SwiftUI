08e3b22fd85cf52703cf96cfd016b834628052e8
Safar Safarov <Safarov.safar@hotmail.com>
2022-09-23 18:17:13 +0500

    ✨

---
+++ Pomodoro-SwiftUI/Pomodoro_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_SwiftUIApp.swift
+//  Pomodoro-SwiftUI
+//
+//  Created by Safar Safarov on 23/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
